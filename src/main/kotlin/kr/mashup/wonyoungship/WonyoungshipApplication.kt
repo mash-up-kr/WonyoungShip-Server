5945e9aba77d672eaf65f4963bcdc2bcb765343f
KyungHwanKim <kkkh0712@naver.com>
2025-04-10 00:04:10 +0900

    setup

---
+++ src/main/kotlin/kr/mashup/wonyoungship/WonyoungshipApplication.kt
@@ -0,0 +1,11 @@
+package kr.mashup.wonyoungship
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WonyoungshipApplication
+
+fun main(args: Array<String>) {
+	runApplication<WonyoungshipApplication>(*args)
+}
---
+++ src/test/kotlin/kr/mashup/wonyoungship/WonyoungshipApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.mashup.wonyoungship
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WonyoungshipApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
