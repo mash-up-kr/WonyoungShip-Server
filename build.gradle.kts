6d3a64f3084dd506f0e4df6e98899e7faf61cd84
thguss <thgus345@naver.com>
2025-05-24 10:25:15 +0900

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,16 @@ subprojects {
 	apply(plugin = "org.springframework.boot")
 	apply(plugin = "io.spring.dependency-management")
 
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter-actuator")
+		implementation("org.springframework.boot:spring-boot-starter-web")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	}
+
 	java {
 		toolchain {
 			languageVersion = JavaLanguageVersion.of(21)
@@ -34,16 +44,6 @@ subprojects {
 		compilerOptions {
 			freeCompilerArgs.addAll("-Xjsr305=strict")
 		}
-	}
-
-	dependencies {
-		implementation("org.springframework.boot:spring-boot-starter-actuator")
-		implementation("org.springframework.boot:spring-boot-starter-web")
-		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	}
 
 	tasks.withType<Test> {
