20311c300631e2fd8bf240086ca801c80fbdde67
thguss <thgus345@naver.com>
2025-06-28 17:56:53 +0900

    update

--- doong2-http/src/main/kotlin/wyship/doong2/http/HttpUrls.kt
+++ doong2-http/src/main/kotlin/wyship/doong2/http/HttpUrls.kt
@@ -2,4 +2,4 @@ package wyship.doong2.http
 
 const val LOGIN_URL = "/api/v1/auth/kakao"
 const val LANDING_CONTENT_URL = "/api/v1/landing"
-const val LETTER_URL = "/api/v1/letter"
+const val LETTER_URL = "/api/v1/letters"
