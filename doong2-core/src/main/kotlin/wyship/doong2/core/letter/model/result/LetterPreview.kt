b373276171049c41d41f2a821d1d8a230e0f17f7
thguss <thgus345@naver.com>
2025-06-29 02:33:45 +0900

    update

--- doong2-core/src/main/kotlin/wyship/doong2/core/letter/model/result/LetterPreview.kt
+++ doong2-core/src/main/kotlin/wyship/doong2/core/letter/model/result/LetterPreview.kt
@@ -1,15 +1,18 @@
 package wyship.doong2.core.letter.model.result
 
 import wyship.doong2.core.letter.port.Letter
+import java.time.LocalDate
 
 data class LetterPreview(
     val letterId: Long,
     val content: String?,
+    val scheduleDate: LocalDate,
 ) {
     companion object {
         fun from(letter: Letter): LetterPreview = LetterPreview(
             letterId = letter.id,
             content = letter.content.takeIf { letter.viewed },
+            scheduleDate = letter.scheduleDate,
         )
     }
 }
--- doong2-http/src/main/kotlin/wyship/doong2/http/letter/model/LetterPreviewResponse.kt
+++ doong2-http/src/main/kotlin/wyship/doong2/http/letter/model/LetterPreviewResponse.kt
@@ -1,15 +1,18 @@
 package wyship.doong2.http.letter.model
 
 import wyship.doong2.core.letter.model.result.LetterPreview
+import java.time.LocalDate
 
 data class LetterPreviewResponse(
     val letterId: Long,
     val content: String?,
+    val scheduleDate: LocalDate,
 ) {
     companion object {
         fun from(letter: LetterPreview): LetterPreviewResponse = LetterPreviewResponse(
             letterId = letter.letterId,
             content = letter.content,
+            scheduleDate = letter.scheduleDate,
         )
     }
 }
